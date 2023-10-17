7bf61c371efedee2fbb54fa1f934548d101df34a
bkvdm <vadim@991199.ru>
2023-10-17 14:57:08 +0300

    Коллекции: листы и сеты. (Переработка курсового проекта в String Boot) https://skyengpublic.notion.site/2-5-0b2646a45c2a46e6bd2f2467c3febc2a

---
+++ src/main/java/tel/bvm/homework203/Application.java
@@ -0,0 +1,13 @@
+package tel.bvm.homework203;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
